5aaa7b828c5df198b3f3796c2715acd819e51dad
Allex Aldir Campos de Assis <allex.assis@grupofm.local>
2023-05-29 13:37:05 -0300

    Feature => .editorconfig/eslint
    Configurações de editores de texto

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 const serve = express(); //Configuração para a criação de rotas do servidor
 
-serve.get('/', (req, res) => { //Aqui coloca o metodo o qual irá ser feito
-    return res.send('Hello world') //Resposta sendo enviada
-})
+interface Teste {
 
-export { serve }
+}
+
+serve.get('/', (req, res) => { //Aqui coloca o metodo o qual irá ser feito
+  return res.send('Hello world'); //Resposta sendo enviada
+});
+
+export { serve };
