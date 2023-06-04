f1efde7591c131fe4dcf18b8af76bfcd33637cf9
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-06-04 17:57:12 -0300

    Add => controllers/database/services/middlewares
    Colocando pastas no projeto

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,7 +8,7 @@ router.get('/', (req, res) => { //Aqui coloca o metodo o qual irá ser feito
 })
 
 router.post('/pessoa', (req, res) => { //Aqui recebe os dados
-  console.log(req.body) //passa os dados para o body
+  //console.log(req.body) //passa os dados para o body
   // console.log(req.params.id) Aqui passa as informações em um parâmetro
 
   return res.status(StatusCodes.ACCEPTED).send(req.body); //Resposta sendo enviada
