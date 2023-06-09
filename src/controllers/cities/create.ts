9138c46ccad884e30feb49f9a979cc3a90e27b62
Allex Aldir Campos de Assis <allex.assis@grupofm.local>
2023-06-09 11:05:48 -0300

    Add => Controllers
    Ajustes no controller cidades

--- src/controllers/cities/create.ts
+++ src/controllers/cities/create.ts
@@ -1,23 +1,42 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
 //Criação da primeira controller:
 
+//Interface para tipar o objeto:
 interface City {
-  nome: string,
-  idade: number
+  nome: string;
 }
+
+//Validando o atributo nome
+const validation: yup.Schema<City> = yup.object().shape({
+  nome: yup.string().required().min(2)
+});
+
+export const city = async (req: Request<{}, {}, City>, res: Response) => {
+  //Reposta da requisicao
+  const objetoGlobal = {
+    status: 200,
+    mensage: 'ok aceito'
+  }
+
+  let valideDate: City | undefined;
+  //Validação dos campos no front-end
+  try {
+    valideDate = await validation.validate(req.body)
+    //console.log(valideDate)
+    // return response.send('ok')
+  } catch (error) {
+    objetoGlobal.mensage = `Erro ao fazer a requisicao ${error}`
+    objetoGlobal.status = 401;
+
+  } finally {
+    res.status(objetoGlobal.status).send('Efetuada com sucesso');
+  }
+
+};
 
 export const teste = (request: Request, response: Response) => {
   return response.status(StatusCodes.ACCEPTED).send(request.body)
-};
-
-export const city = (request: Request<{}, {}, City>, response: Response) => {
-  return response.send(request.body);
-  //Aqui recebe os dados
-  //console.log(req.body) //passa os dados para o body
-  // console.log(req.params.id) Aqui passa as informações em um parâmetro
-
-  //return res.status(StatusCodes.ACCEPTED).send(req.body); //Resposta sendo enviada
-  //status traz uma semântica de como está o status da resposta
 };
